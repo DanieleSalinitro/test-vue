922997c2c03b9c0101de0edc21943cc7dcbbb83e
DanieleSalinitro <daniele.salinitro@icloud.com>
2024-04-09 12:39:16 +0200

    modifiche

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <main>
+    <img src="../assets/img/image_720.png" alt="">
+    </main>
+</template>
+
+<script>
+    export default {
+        name: 'MainComponent',
+        data() {
+            return {
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
